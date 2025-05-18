399c62ec2fbe1c035aa970c36bf67c6adfe1d467
Natalia Matias <nataliamps@outlook.com>
2025-05-18 18:33:34 -0300

    Atualizando layout da tela de cadastro e apagando alguns commits

--- Conecta_Frontend/Conecta_Prototype/src/pages/Cadastro.jsx
+++ Conecta_Frontend/Conecta_Prototype/src/pages/Cadastro.jsx
@@ -47,89 +47,93 @@ function Cadastro() {
   };
 
   return (
-      <div 
-        className="d-flex justify-content-center align-items-center min-vh-100"
-        style={{ background: 'linear-gradient(135deg, #1C88EB, #4014F0)'}}
-      >
-        
-      <div 
-        className="card shadow-lg rounded-4 p-4 w-100"
-        style={{maxWidth: '420px', backgroundColor: '#ffffffee', border: '1px solid #1AEBDB'}}
-      >
+  <div className="d-flex align-items-center justify-content-center min-vh-100 p-3"
+       style={{background: "linear-gradient(135deg, #1C88EB, #4014F0)"}}>
+    
+    <div className="shadow-lg p-4 rounded-4"
+         style={{width: "100%", maxWidth: "600px", backgroundColor: "#ffffffee", border: "1px solid #1AEBDB"}}>
 
-      <div 
-      //Div titulo e subtitulo, fontes, cores e espaçamento padronizadas com tela login
-        className="text-center mb-4">
-        <h2 className="fw-bold" style={{ color: '#1630F0' }}>Cadastro</h2>
-        <p className="text-muted small">Cadastre suas informações de usuário</p>
-      </div>  
+      <div className="text-center mb-4">
+        <h2 className="fw-bold" style={{ color: '#1630F0' }}>Cadastro de Usuário</h2>
+      </div>
 
       <form onSubmit={handleSubmit}>
-        <div hidden>
-        {/*
-          Mudar className="mb-4" para ="mb-3", mantendo o espaçamento do bootstrap padronizado nas pgs do app
-          Acrescentar os ids no imput e htmlfor no label garante padronização, acessibilidade e usabilidade
-          No label acrescenta a cor e fonte
-          Separar as Divs por campo de informação. Ex: div 1 - nome, div 2 - email, div 3 - endereço, etc...
-        */}
+
+        {/* Primeira linha - Nome e CPF */}
+        <div className="row">
+          <div className="col-md-6 mb-3">
+            <label htmlFor="nome" className="form-label fw-semibold" style={{ color: '#1C88EB' }}>
+              Nome
+            </label>
+            <input 
+              type="text"
+              className="form-control rounded-3"  
+              id="nome"
+              value={nome} onChange={(e) => setNome(e.target.value)}
+              placeholder="Digite seu nome" 
+              required
+            />        
+          </div>
+          <div className="col-md-6 mb-3">
+            <label htmlFor="cpf" className="form-label fw-semibold" style={{ color: '#1C88EB' }}>
+              CPF
+            </label>
+            <input
+              type="text"
+              className="form-control rounded-3"
+              id="cpf"
+              value={cpf} onChange={(e) => setCpf(e.target.value)}
+              placeholder="Digite seu CPF"
+              required
+            />
+          </div>
         </div>
 
-        <div className="mb-3">
-          <label htmlFor="nome" className="form-label fw-semibold" style={{ color: '#1C88EB' }}>
-            Nome
-          </label>
-          <input 
-            type="text"
-            className="form-control rounded-3"  
-            id="nome"
-            value={nome} onChange={(e) => setNome(e.target.value)}
-            placeholder="Digite seu nome" 
-            required
-          />        
-        </div>
-
-        <div className="mb-3">
-          <label htmlFor="email" className="form-label fw-semibold" style={{ color: '#1C88EB' }}>
+        {/* Segunda linha - Email e Senha */}
+        <div className="row">
+          <div className="col-md-6 mb-3">
+            <label htmlFor="email" className="form-label fw-semibold" style={{ color: '#1C88EB' }}>
               Email
-          </label>
-          <input 
-            type="email" 
-            className="form-control rounded-3" 
-            id="email"
-            value={email} onChange={(e) => setEmail(e.target.value)}
-            placeholder="Digite seu email"
-            required 
-          />
+            </label>
+            <input 
+              type="email" 
+              className="form-control rounded-3" 
+              id="email"
+              value={email} onChange={(e) => setEmail(e.target.value)}
+              placeholder="Digite seu email"
+              required 
+            />
+          </div>
+          <div className="col-md-6 mb-3">
+            <label htmlFor="senha" className="form-label fw-semibold" style={{ color: '#1C88EB' }}>
+              Senha
+            </label>
+            <input
+              type="password"
+              className="form-control rounded-3"
+              id="senha"
+              value={senha} onChange={(e) => setSenha(e.target.value)}
+              placeholder="Defina uma senha"
+              required
+            />
+          </div>
         </div>
 
+        {/* Data Nascimento (full width) */}
         <div className="mb-3">
-          <label htmlFor="senha" className="form-label fw-semibold" style={{ color: '#1C88EB' }}>
-            Senha
+          <label htmlFor="dataNascimento" className="form-label fw-semibold" style={{ color: '#1C88EB' }}>
+            Data de Nascimento
           </label>
           <input
-            type="password"
+            type="date"
             className="form-control rounded-3"
-            id="senha"
-            value={senha} onChange={(e) => setSenha(e.target.value)}
-            placeholder="Defina uma senha"
+            id="dataNascimento"
+            value={dataNascimento} onChange={(e) => setDataNascimento(e.target.value)}
             required
           />
         </div>
 
-        <div className="mb-3">
-          <label htmlFor="cpf" className="form-label fw-semibold" style={{ color: '#1C88EB' }}>
-            CPF
-          </label>
-          <input
-            type="text"
-            className="form-control rounded-3"
-            id="cpf"
-            value={cpf} onChange={(e) => setCpf(e.target.value)}
-            placeholder="Digite seu CPF"
-            required
-          />
-        </div>
-
+        {/* Endereço (full width) */}
         <div className="mb-3">
           <label htmlFor="endereco" className="form-label fw-semibold" style={{ color: '#1C88EB' }}>
             Endereço
@@ -144,51 +148,41 @@ function Cadastro() {
           />
         </div>
 
-        <div className="mb-3">
-          <label htmlFor="cidade" className="form-label fw-semibold" style={{ color: '#1C88EB' }}>
-            Cidade
-          </label>
-          <input
-            type="text"
-            className="form-control rounded-3"
-            id="cidade"
-            value={cidade} onChange={(e) => setCidade(e.target.value)}
-            placeholder="Digite sua cidade"
-            required
-          />
+        {/* Quarta linha - Cidade e Estado */}
+        <div className="row">
+          <div className="col-md-6 mb-3">
+            <label htmlFor="cidade" className="form-label fw-semibold" style={{ color: '#1C88EB' }}>
+              Cidade
+            </label>
+            <input
+              type="text"
+              className="form-control rounded-3"
+              id="cidade"
+              value={cidade} onChange={(e) => setCidade(e.target.value)}
+              placeholder="Digite sua cidade"
+              required
+            />
+          </div>
+          <div className="col-md-6 mb-3">
+            <label htmlFor="estado" className="form-label fw-semibold" style={{ color: '#1C88EB' }}>
+              Estado
+            </label>
+            <input
+              type="text"
+              className="form-control rounded-3"
+              id="estado"
+              value={estado} onChange={(e) => setEstado(e.target.value)}
+              placeholder="Digite seu estado"
+              required
+            />
+          </div>
         </div>
 
-        <div className="mb-3">
-          <label htmlFor="estado" className="form-label fw-semibold" style={{ color: '#1C88EB' }}>
-            Estado
-          </label>
-          <input
-            type="text"
-            className="form-control rounded-3"
-            id="estado"
-            value={estado} onChange={(e) => setEstado(e.target.value)}
-            placeholder="Digite seu estado"
-            required
-          />
-        </div>
-
-        <div className="mb-3">
-          <label htmlFor="dataNascimento" className="form-label fw-semibold" style={{ color: '#1C88EB' }}>
-            Data de Nascimento
-          </label>
-          <input
-            type="text"
-            className="form-control rounded-3"
-            id="dataNascimento"
-            value={dataNascimento} onChange={(e) => setDataNascimento(e.target.value)}
-            placeholder="Nascimento AAAA-MM-DD"
-            required
-          />
-        </div>
-
+        {/* Áreas de Atuação (full width) */}
         <div className="mb-3">
           <label htmlFor="areasDeAtuacao" className="form-label fw-semibold" style={{ color: '#1C88EB' }}>
-            Áreas de Atuação (separar por vírgula)</label>
+            Áreas de Atuação (separar por vírgula)
+          </label>
           <input
             type="text"
             className="form-control rounded-3"
@@ -199,26 +193,24 @@ function Cadastro() {
           />
         </div>
 
-        <button
-          type="submit"
-          className="btn w-100 rounded-3 fw-semibold"
-          style={{ backgroundColor: '#1AEBDB', border: 'none', color: '#fff'}}
+        <button type="submit" className="btn w-100 rounded-3 fw-semibold py-2 mt-3"
+          style={{ backgroundColor: '#1AEBDB', border: 'none', color: '#fff' }}
         >
-            <i className="bi bi-box-arrow-in-right me-2"></i> Cadastrar
+          Cadastrar
         </button>
       </form>
 
       <div className="text-center mt-4">
         <p className="mb-0 small">
           Já tem uma conta?{' '}
-            <a href="./" className="text-decoration-none fw-semibold" style={{ color: '#1E78EB' }}>
-              Faça login
-            </a>
+          <a href="./" className="text-decoration-none fw-semibold" style={{ color: '#1E78EB' }}>
+            Faça login
+          </a>
         </p>
       </div>
     </div>
   </div>
-  );
+);
 }
 
 export default Cadastro;
